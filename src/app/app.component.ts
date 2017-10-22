9e008084f5cc1fb359fc834fd3d13d79ccf9a9a0
littlesquid <littlesquid.1991@gmail.com>
2017-10-22 09:24:01 +0700

    noei

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   count = 0;
-
+  tabamt = 0;
+  total = 0;
+ 
 doCount(){
   this.count += 1;
+  this.calculate();
 }
 doReset(){
   this.count = 0;
+  this.tabamt = 0;
+  this.total = 0;
+}
+
+calculate(){
+  this.total = this.count*this.tabamt;
 }
 }
